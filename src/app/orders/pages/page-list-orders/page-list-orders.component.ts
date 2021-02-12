30baba6595a8b2637802f158d4b8b41ede206083
christophe gueroult <christophegueroult@gmail.com>
2021-02-12 15:53:01 +0100

    vendredi midi

--- src/app/orders/pages/page-list-orders/page-list-orders.component.ts
+++ src/app/orders/pages/page-list-orders/page-list-orders.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Observable, Subscription } from 'rxjs';
+import { BehaviorSubject, Observable, Subject, Subscription } from 'rxjs';
 import { Order } from 'src/app/core/models/order';
 import { OrdersService } from 'src/app/core/services/orders.service';
 
@@ -9,6 +9,8 @@ import { OrdersService } from 'src/app/core/services/orders.service';
   styleUrls: ['./page-list-orders.component.scss']
 })
 export class PageListOrdersComponent implements OnInit, OnDestroy {
+  private subj = new Subject<any>();
+  private behav = new BehaviorSubject<any>('Jules');
   // example d'observable perso
   private testObservablePerso: Observable<any> = new Observable<any>((listXofSubscribes) => {
     listXofSubscribes.next('string');
@@ -17,15 +19,16 @@ export class PageListOrdersComponent implements OnInit, OnDestroy {
   private sub!: Subscription;
   public title = 'All orders';
   // public collection!: Order[];
-  public collection$!: Observable<Order[]>;
+  public collection$: Subject<Order[]> = new Subject<Order[]>();
   public headers!: string[];
   constructor(private ordersService: OrdersService) {
-    // this.ordersService.collection.subscribe((datas) => {
-    //   this.collection = datas;
-    //   // console.log(datas);
-    // });
-    this.collection$ = this.ordersService.collection;
+    this.ordersService.collection.subscribe((datas) => {
+      this.collection$.next(datas);
+      // console.log(datas);
+    });
+    // this.collection$ = this.ordersService.collection;
     this.headers = [
+      'Action',
       'Type',
       'Client',
       'NbJours',
@@ -42,6 +45,19 @@ export class PageListOrdersComponent implements OnInit, OnDestroy {
         console.log(datas);
       }
     );
+
+    // ex d'utilisatin de Subject
+    this.subj.subscribe((data) => {
+      console.log(data);
+    });
+    this.subj.next('christophe');
+    this.subj.next('aby');
+    // ex d'utilisatin de BejaviorSubject
+    this.behav.subscribe((data) => {
+      console.log(data);
+    });
+    this.behav.next('Audrey');
+    this.behav.next('Khalil');
   }
 
   public openPopUp(): void {
